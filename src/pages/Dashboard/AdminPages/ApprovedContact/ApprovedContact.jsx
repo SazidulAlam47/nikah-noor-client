24d7c016fd7730f91e728ead7f045973ffee0ff4
Sazidul Alam <sazidulalam20@gmail.com>
2024-06-05 23:37:08 +0600

    Approved Contact not found text

--- src/pages/Dashboard/AdminPages/ApprovedContact/ApprovedContact.jsx
+++ src/pages/Dashboard/AdminPages/ApprovedContact/ApprovedContact.jsx
@@ -54,77 +54,90 @@ const ApprovedContact = () => {
                 subtitle="View and manage contact requests that have been approved by the admin"
             />
             <div className="mt-8">
-                <Card className="h-fit w-full overflow-x-auto">
-                    <table className="w-full min-w-max table-auto text-left">
-                        <thead>
-                            <tr>
-                                {TABLE_HEAD.map((head) => (
-                                    <th
-                                        key={head}
-                                        className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
-                                    >
-                                        <Typography
-                                            variant="small"
-                                            color="blue-gray"
-                                            className="font-normal leading-none opacity-70"
+                {requests?.length ? (
+                    <Card className="h-fit w-full overflow-x-auto">
+                        <table className="w-full min-w-max table-auto text-left">
+                            <thead>
+                                <tr>
+                                    {TABLE_HEAD.map((head) => (
+                                        <th
+                                            key={head}
+                                            className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
                                         >
-                                            {head}
-                                        </Typography>
-                                    </th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {requests?.map(
-                                ({ _id, userName, userEmail, requestedId }) => (
-                                    <tr
-                                        key={_id}
-                                        className="even:bg-blue-gray-50/50"
-                                    >
-                                        <td className="p-4">
                                             <Typography
                                                 variant="small"
                                                 color="blue-gray"
-                                                className="font-normal"
+                                                className="font-normal leading-none opacity-70"
                                             >
-                                                {userName}
+                                                {head}
                                             </Typography>
-                                        </td>
-                                        <td className="p-4">
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal"
-                                            >
-                                                {userEmail}
-                                            </Typography>
-                                        </td>
-                                        <td className="p-4">
-                                            <Typography
-                                                variant="small"
-                                                color="blue-gray"
-                                                className="font-normal"
-                                            >
-                                                {requestedId}
-                                            </Typography>
-                                        </td>
+                                        </th>
+                                    ))}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {requests?.map(
+                                    ({
+                                        _id,
+                                        userName,
+                                        userEmail,
+                                        requestedId,
+                                    }) => (
+                                        <tr
+                                            key={_id}
+                                            className="even:bg-blue-gray-50/50"
+                                        >
+                                            <td className="p-4">
+                                                <Typography
+                                                    variant="small"
+                                                    color="blue-gray"
+                                                    className="font-normal"
+                                                >
+                                                    {userName}
+                                                </Typography>
+                                            </td>
+                                            <td className="p-4">
+                                                <Typography
+                                                    variant="small"
+                                                    color="blue-gray"
+                                                    className="font-normal"
+                                                >
+                                                    {userEmail}
+                                                </Typography>
+                                            </td>
+                                            <td className="p-4">
+                                                <Typography
+                                                    variant="small"
+                                                    color="blue-gray"
+                                                    className="font-normal"
+                                                >
+                                                    {requestedId}
+                                                </Typography>
+                                            </td>
 
-                                        <td className="p-4">
-                                            <Button
-                                                onClick={() =>
-                                                    handleApprove(_id)
-                                                }
-                                                size="sm"
-                                            >
-                                                Approve
-                                            </Button>
-                                        </td>
-                                    </tr>
-                                )
-                            )}
-                        </tbody>
-                    </table>
-                </Card>
+                                            <td className="p-4">
+                                                <Button
+                                                    onClick={() =>
+                                                        handleApprove(_id)
+                                                    }
+                                                    size="sm"
+                                                >
+                                                    Approve
+                                                </Button>
+                                            </td>
+                                        </tr>
+                                    )
+                                )}
+                            </tbody>
+                        </table>
+                    </Card>
+                ) : (
+                    <div className="flex items-center justify-center h-[55vh]">
+                        <Typography variant="lead">
+                            There is no Contact Requests at this moment
+                        </Typography>
+                    </div>
+                )}
             </div>
         </>
     );
