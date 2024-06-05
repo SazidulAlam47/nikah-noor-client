efd50a00bc621670f151913406ef1d62be862dab
Sazidul Alam <sazidulalam20@gmail.com>
2024-06-06 02:07:10 +0600

    deploy fix

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // export const baseUrl = "http://localhost:5000";
-export const baseUrl = "https://nikah-noor-server.vercel.app/";
+export const baseUrl = "https://nikah-noor-server.vercel.app";
 
 const axiosPublic = axios.create({
     baseURL: baseUrl,
